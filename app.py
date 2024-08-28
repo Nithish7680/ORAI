40241000b44b98f94b65b79bc74b1ed779f3185f
Nithish7680 <89068206+Nithish7680@users.noreply.github.com>
2024-08-28 20:56:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from datetime import datetime
+from pyzbar.pyzbar import decode
+from PIL import Image
+import requests
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -32,7 +36,43 @@ def hello_there(name=None):
 def get_data():
     """Serve a static JSON file."""
     return app.send_static_file("data.json")
-
+    
+@app.route('/decode-barcode', methods=['POST'])
+def decode_barcode():
+    data = request.get_json()
+   
+ 
+    # if 'url' not in data:
+    #     return jsonify({"error": "Missing 'url' key in JSON body"}), 400
+    # image_url = "https://fpi.branding-element.com/prod/34782/WA_PUBLIC_ATTACHMENT/88d9ce69_903b_4a6e_bde6_45dd214d407d.jpeg-QsUGG.jpeg"
+    image_url = data['url']
+   
+ 
+    try:
+       
+        response = requests.get(image_url)
+        response.raise_for_status()
+ 
+       
+        img_bytes = BytesIO(response.content)
+        img_pil = Image.open(img_bytes)
+ 
+        results = decode(img_pil)
+ 
+        if not results:
+            return jsonify({"error": "No barcodes or QR codes found in the image"}), 404
+ 
+        barcode = results[0]
+        decoded_data = {
+            "Code": barcode.data.decode('utf-8'),
+            "type": barcode.type
+        }
+        return jsonify(decoded_data)
+ 
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": f"Request error: {str(e)}"}), 400
+    except Exception as e:
+        return jsonify({"error": f"An error occurred: {str(e)}"}), 500
 
 
 if __name__ == "__main__":
